134259f04bd5c24c5ad327c53638d846e93a4c45
Nitesh Yadav <ny040021@gmail.com>
2024-02-11 04:03:48 +0530

    _

--- netlify/functions/info.js
+++ netlify/functions/info.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
     await connectDB();
     return {
       statusCode: 200,
-      body: JSON.stringify({ ...event }),
+      body: JSON.stringify({ ...event }, null, 2),
     };
   } catch (error) {
     return {
