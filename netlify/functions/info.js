15ed870b96fed175ba04628a632666e086432afd
Nitesh Yadav <Nitesh.Y@ZIFOCORP.COM>
2024-02-10 17:30:20 +0530

    _

--- netlify/functions/info.js
+++ netlify/functions/info.js
@@ -1,24 +1,6 @@
-import lambdaMultipart from "lambda-multipart";
-import formidable from "formidable";
-
 exports.handler = async (event) => {
   try {
-    const formData = await lambdaMultipart.parse(event);
-    const form = new formidable.IncomingForm();
-
-    form.parse(formData, (err, fields, files) => {
-      if (err) {
-        return {
-          statusCode: 500,
-          body: JSON.stringify({ error: "Error parsing form data" }),
-        };
-      }
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ fields, files }),
-      };
-    });
+    return new Response(JSON.stringify(event));
   } catch (error) {
     return {
       statusCode: 500,
