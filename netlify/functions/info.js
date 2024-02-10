f737338a19e727dce000aaea6350f6b975b9b44c
Nitesh Yadav <Nitesh.Y@ZIFOCORP.COM>
2024-02-10 17:02:18 +0530

    _

---
+++ netlify/functions/info.js
@@ -0,0 +1,10 @@
+import formidable from "formidable";
+
+export default async (req, context) => {
+  try {
+    return new Response(String(Object.getPrototypeOf(req)));
+    // const [fields, files] = await formidable({}).parse(req);
+  } catch (e) {
+    return new Response({ message: e.message }, { status: 500 });
+  }
+};
