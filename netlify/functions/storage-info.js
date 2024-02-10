24be57a40b2689dbf8e215734f486f7f87e554d0
Nitesh Yadav <suboptimalmachine@gmail.com>
2024-02-10 15:53:19 +0530

    _

---
+++ netlify/functions/storage-info.js
@@ -0,0 +1,13 @@
+import { check } from "diskusage";
+import os from "os";
+
+const path = os.platform() === "win32" ? "C:" : "/";
+
+export default async (req, context) => {
+  check(path, (err, info) => {
+    if (err) {
+      return new Response(err.message, { status: 500 })
+    }
+    return new Response(info);
+  })
+};
