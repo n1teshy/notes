d62f16334ad21e6e78cfec7dcdf79101f84eaadd
Nitesh Yadav <Nitesh.Y@ZIFOCORP.COM>
2024-02-10 16:11:48 +0530

    _

--- netlify/functions/storage-info.js
+++ netlify/functions/storage-info.js
@@ -1,14 +1,7 @@
-import { check } from "diskusage";
 import os from "os";
 
 const path = os.platform() === "win32" ? "C:" : "./";
 
 export default async (req, context) => {
-  // return new Response(JSON.stringify({ path }));
-  check(path, (err, info) => {
-    if (err) {
-      return new Response("error", { status: 500 });
-    }
-    return new Response("info");
-  });
+  return new Response(String(typeof req.body));
 };
