f942128cc79dabcc31c365556aa8c18f15eefaa7
Nitesh Yadav <Nitesh.Y@ZIFOCORP.COM>
2024-02-10 16:48:25 +0530

    _

--- netlify/functions/storage-info.js
+++ netlify/functions/storage-info.js
@@ -1,14 +1,8 @@
-import fs from "fs/promises";
-
 export default async (req, context) => {
-  let status = 200;
-  let message = "everything went well.";
   try {
-    await fs.writeFile("file.txt", "hello I am a file");
+    return new Response(req.body);
   } catch (e) {
     status = 500;
     message = e.message;
-  } finally {
-    return new Response(message, { status });
   }
 };
