f8309c574599c39b50169488dc41e26f103f38b5
Nitesh Yadav <Nitesh.Y@ZIFOCORP.COM>
2024-02-10 17:04:37 +0530

    _

--- netlify/functions/info.js
+++ netlify/functions/info.js
@@ -4,6 +4,7 @@ export default async (req, context) => {
   try {
     // return new Response(String(Object.getPrototypeOf(req)));
     const [fields, files] = await formidable({}).parse(req);
+    return new Response(JSON.stringify({ fields, files }));
   } catch (e) {
     return new Response({ message: e.message }, { status: 500 });
   }
