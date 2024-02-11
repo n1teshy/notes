861e87041743eca2061f2c57c19f2bddef9e08a7
Nitesh Yadav <ny040021@gmail.com>
2024-02-11 20:34:39 +0530

    _

--- netlify/functions/conversation.js
+++ netlify/functions/conversation.js
@@ -2,29 +2,29 @@ import { Conversation } from "../models/conversation.js";
 import { onRequest } from "../utils/request.js";
 import { ConversationValidator } from "../utils/validation.js";
 import { makeResponse, statuses } from "../utils/response.js";
+import { AppError } from "../utils/error.js";
 
 const validator = new ConversationValidator();
 
 exports.handler = async (req) => {
   try {
     const method = req.httpMethod;
+    if (method !== "GET" && method !== "POST") {
+      throw new AppError(statuses.FORBIDDEN, "Nah cuh, wrong method.");
+    }
+    await onRequest(req, true);
     if (method === "GET") {
-      const conversations = await Conversation.find();
-      return makeResponse(conversations.map(async (c) => await c.toJSON()));
+      const convos = await Conversation.find({
+        participants: { $elemMatch: { _id: req.user.id } },
+      });
+      return convos.map(async (c) => await c.toJSON());
     }
-    if (method === "POST") {
-      await onRequest(req);
-      const validationErrors = await validator.asyncValidate(req.body);
-      if (validationErrors) {
-        return makeResponse(validationErrors, statuses.UNPROCESSABLE);
-      }
-      const conversation = await Conversation.create(req.body);
-      return makeResponse(conversation.toJSON());
+    const validationErrors = await validator.asyncValidate(req.body);
+    if (validationErrors) {
+      return makeResponse(validationErrors, statuses.UNPROCESSABLE);
     }
-    return makeResponse(
-      { message: "Nah bro, wrong method." },
-      statuses.FORBIDDEN
-    );
+    const conversation = await Conversation.create(req.body);
+    return makeResponse(await conversation.toJSON());
   } catch (error) {
     return makeResponse({ message: error.message }, error.status || 500);
   }
