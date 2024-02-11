135f1fafb86e754ad6d8e6dcb1ed830020b65fbb
Nitesh Yadav <ny040021@gmail.com>
2024-02-11 19:35:56 +0530

    _

--- netlify/functions/info.js
+++ netlify/functions/info.js
@@ -1,17 +1,16 @@
 import { makeResponse } from "../utils/response.js";
 
-const data = { data: null };
+const data = { utc: null, indian: null };
 
 exports.handler = async (req, context) => {
   try {
     if (data.data === null) {
       const date = new Date();
-      data.data = `${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`;
+      data.utc = `${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`;
+      data.indian = date.toLocaleDateString("en-IN");
       return makeResponse({ message: "set" });
     }
-    const copy = Object.assign({}, data);
-    data.data = null;
-    return makeResponse(copy);
+    return makeResponse(data);
   } catch (error) {
     return makeResponse({ message: error.message }, error.status || 500);
   }
