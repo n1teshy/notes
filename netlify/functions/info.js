5647a9bcb25369be95eb16ed82ca181e1b6afb08
Nitesh Yadav <ny040021@gmail.com>
2024-02-11 19:03:36 +0530

    _

--- netlify/functions/info.js
+++ netlify/functions/info.js
@@ -1,13 +1,8 @@
-import { connectDB } from "../utils/db.js";
 import { makeResponse } from "../utils/response.js";
 
 exports.handler = async (req, context) => {
   try {
-    await connectDB();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ req, context }, null, 2),
-    };
+    return makeResponse({ req, context });
   } catch (error) {
     return makeResponse({ message: error.message }, error.status || 500);
   }
