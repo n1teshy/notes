0859555f992f546d76e6a5d4b867ed3a99a729b5
Nitesh Yadav <ny040021@gmail.com>
2024-02-11 18:37:22 +0530

    _

--- netlify/utils/request.js
+++ netlify/utils/request.js
@@ -1,9 +1,13 @@
+import { decrypt } from "./crypt.js";
 import { connectDB } from "./db.js";
 
 export async function onRequest(req) {
   req.body = JSON.parse(req.body);
   req.queries = Object.fromEntries(
-    "key=value&key2=value".split("&").map((query) => query.split("="))
+    req.rawQuery.split("&").map((query) => query.split("="))
   );
+  if (req.headers.authorization) {
+    req.user = JSON.parse(decrypt(req.headers.authorization));
+  }
   await connectDB();
 }
