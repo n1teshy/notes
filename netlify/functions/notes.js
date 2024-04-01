50966bba8980e89d0792077a07afb6b4040a1301
Nitesh Yadav <nitesh.y@zifornd.com>
2024-04-01 15:43:07 +0530

    _

--- netlify/functions/notes.js
+++ netlify/functions/notes.js
@@ -8,7 +8,7 @@ const validator = new NoteValidator();
 
 exports.handler = async (req) => {
   try {
-    const handler = handlers.find((item) => new RegExp(item[0]).test(req.path))
+    const handler = handlers.find((item) => new RegExp(item[0]).test(req.path));
     if (handler) {
       return await handler[1](req);
     } else {
@@ -26,6 +26,7 @@ async function notes(req) {
   }
   await onRequest(req);
   if (method == "POST") {
+    return makeResponse(req.body);
     const errors = await validator.asyncValidate(req.body);
     if (errors) {
       return makeResponse(errors, statuses.UNPROCESSABLE);
@@ -33,14 +34,16 @@ async function notes(req) {
     const note = await Note.create(req.body);
     return makeResponse(note.toJSON());
   }
-  const notes = await Note.find().skip(req.queries.skip ?? 0).limit(req.queries.limit ?? 1000);
+  const notes = await Note.find()
+    .skip(req.queries.skip ?? 0)
+    .limit(req.queries.limit ?? 1000);
   return makeResponse(notes.map((note) => note.toJSON()));
 }
 
 async function note(req) {
   const method = req.httpMethod;
   if (method !== "GET" && method !== "PUT" && method !== "DELETE") {
-    throw AppError(statuses.UNPROCESSABLE, "wrong method fam.")
+    throw AppError(statuses.UNPROCESSABLE, "wrong method fam.");
   }
   await onRequest(req);
   const noteId = req.path.replace(/\/$/, "").split("/").at(-1);
@@ -55,14 +58,16 @@ async function note(req) {
     if (errors) {
       return makeResponse(errors, statuses.UNPROCESSABLE);
     }
-    const updated = await Note.findByIdAndUpdate(noteId, req.body, { new: true });
+    const updated = await Note.findByIdAndUpdate(noteId, req.body, {
+      new: true,
+    });
     return makeResponse(updated.toJSON());
   }
   await Note.findByIdAndDelete(noteId);
-  return makeResponse({ message: "deleted" })
+  return makeResponse({ message: "deleted" });
 }
 
 const handlers = [
   ["/.netlify/functions/notes/?$", notes],
   ["/.netlify/functions/notes/[A-Fa-f0-9]{24}/?$", note],
-]
+];
