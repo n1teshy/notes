935d657bb263024bbbcaf4241ad0900c70d6d490
NItesh Yadav <78202277+n1teshy@users.noreply.github.com>
2024-07-21 13:41:41 +0530

    use DATABASE_URI env-var to connect to database

--- netlify/utils/db.js
+++ netlify/utils/db.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-  const DTAABASE_URI = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@cluster0.z0byrzc.mongodb.net/`;
-  await mongoose.connect(DTAABASE_URI);
+  await mongoose.connect(process.env.DATABASE_URI);
 }
